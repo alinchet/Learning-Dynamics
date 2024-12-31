f5dfad4c84e81df74512e93117efd8eb3abcb49d
zmoerema <zoe.moeremans@ulb.be>
2024-12-31 17:25:44 +0100

    [FIX] strategy enum

--- src/constants.py
+++ src/constants.py
@@ -2,18 +2,18 @@ from enum import Enum
 from src.config import b, c
 
 class Strategy(Enum):
-    ALTRUIST = 0,
-    PAROCHIALIST = 1,
+    ALTRUIST = 0
+    PAROCHIALIST = 1
     EGOIST = 2    
 
 A_IN_MATRIX = [
     [b-c, b-c, -c],
     [b-c, b-c, -c],
-    [b, b, 0],
+    [b, b, 0]
 ]
 
 A_OUT_MATRIX = [
     [b-c, -c, -c],
     [b, 0, 0],
-    [b, 0, 0],
+    [b, 0, 0]
 ]
