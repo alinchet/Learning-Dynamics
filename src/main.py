5e4b76299eb776f7283aea8a93b6ead29ceba8cf
AdelNehili <adel@LAPTOP-435R1L8O>
2025-01-05 22:43:58 +0100

    update plot

--- src/main.py
+++ src/main.py
@@ -11,18 +11,18 @@ if __name__ == '__main__':
 
     (bc_ratio_values,probabilities),x_label,y_label = tools.run_bc_simulation(nbr_runs)
     tools.save_graphs(bc_ratio_values,probabilities,x_label,y_label)
+    """
+        (lambda_mig_values,probabilities),x_label,y_label = tools.run_lambda_mig_simulation(nbr_runs)
+        tools.save_graphs(lambda_mig_values,probabilities,x_label,y_label)
 
-    (lambda_mig_values,probabilities),x_label,y_label = tools.run_lambda_mig_simulation(nbr_runs)
-    tools.save_graphs(lambda_mig_values,probabilities,x_label,y_label)
+        (n_values,probabilities,x_label),y_label = tools.run_n_simulation(nbr_runs)
+        tools.save_graphs(n_values,probabilities,x_label,y_label)
 
-    (n_values,probabilities,x_label),y_label = tools.run_n_simulation(nbr_runs)
-    tools.save_graphs(n_values,probabilities,x_label,y_label)
+        (m_values,probabilities),x_label,y_label = tools.run_m_simulation(nbr_runs)
+        tools.save_graphs(m_values,probabilities,x_label,y_label)
 
-    (m_values,probabilities),x_label,y_label = tools.run_m_simulation(nbr_runs)
-    tools.save_graphs(m_values,probabilities,x_label,y_label)
+        (alpha_values,probabilities),x_label,y_label = tools.run_alpha_simulation(nbr_runs)
+        tools.save_graphs(alpha_values,probabilities,x_label,y_label)
 
-    (alpha_values,probabilities),x_label,y_label = tools.run_alpha_simulation(nbr_runs)
-    tools.save_graphs(alpha_values,probabilities,x_label,y_label)
-
-    
-    
+        """
+        
